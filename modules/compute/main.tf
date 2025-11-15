d83b1873088b2580b2deead75639abe2c7c46f2a
Pablo D. <89665590+pdelaroche@users.noreply.github.com>
2025-11-15 18:21:19 +0100

    Refactor/infrastructure optimization (#5)
    * refactor: configure compute module
    
    * configure compute module
    
    * temporarily comment compute module
    
    * temporarily comment compute module
    
    * refactor: add Terraform backend and provider configuration files

---
+++ modules/compute/main.tf
@@ -0,0 +1,8 @@
+# resource "ec2_instance" "web_server" {
+#   ami           = var.ami_id
+#   instance_type = var.instance_type
+
+#   tags = {
+#     Name = "WebServer"
+#   }
+# }
---
+++ modules/compute/outputs.tf
@@ -0,0 +1,4 @@
+# output "web_server_id" {
+#   value = ec2_instance.web_server.id
+#   value = ec2_private_ip.web_server.private_ip
+# }
