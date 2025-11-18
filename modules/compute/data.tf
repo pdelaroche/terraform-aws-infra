00222e60d510d338637c819a380dd3790dde0304
Pablo Delaroche <pabloroche7@gmail.com>
2025-11-18 21:18:54 +0100

    feat: integrate compute module with VPC networking

--- modules/compute/data.tf
+++ modules/compute/data.tf
@@ -1,16 +1,23 @@
-# data "aws_ami" "ubuntu" {
-#   most_recent = true
+data "aws_ami" "ubuntu" {
+  most_recent = true
 
-#   filter {
-#     name   = "name"
-#     values = ["ubuntu/images/hvm-ssd/ubuntu-noble-24.04-amd64-server-*"]
-#   }
+  filter {
+    name = "name"
+    values = [
+      "ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"
+    ]
+  }
 
-#   filter {
-#     name   = "virtualization-type"
-#     values = ["hvm"]
-#   }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
 
-#   owners = ["099720109477"] # Canonical
-# }
+  filter {
+    name   = "state"
+    values = ["available"]
+  }
+
+  owners = ["099720109477"]
+}
 
